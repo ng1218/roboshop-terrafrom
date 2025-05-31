77d98149ec3c280ad1fedfde338169ec4800147b
i340771 <naresh.ganta@sap.com>
2025-05-31 16:11:57 +0530

    test

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 }
 
 module "frontend" {
-  source                     = "./components"
+  source                     = "./component"
   resource_group_name        = var.resource_group_name
   location                   = var.location
   name                       = "frontend"
