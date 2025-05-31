2096deaae0156abe3f391aefd0a05b95ffb48ede
i340771 <naresh.ganta@sap.com>
2025-05-31 23:16:15 +0530

    test

--- component/main.tf
+++ component/main.tf
@@ -63,7 +63,8 @@ resource "null_resource" "web" {
   provisioner "remote-exec" {
     inline = [
       "sudo dnf install python3.12 python3.12-pip -y",
-      "sudo pip3.12 install ansible"
+      "sudo pip3.12 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/ng1218/roboshop-ansible roboshop.yml -e app_name="${var.name}" -e env=dev"
     ]
   }
 }
